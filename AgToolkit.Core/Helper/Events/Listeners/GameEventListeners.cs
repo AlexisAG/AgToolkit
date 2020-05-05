e36edabea2f27cf29b22de23c6893c7b2f4f291a
AlexisAG <agay@amiltone.com>
2020-05-05 20:05:52 +0200

    Remove GameEventListeners
    Documentation have to explain how to use GameEventListeners

--- AgToolkit.Core/Helper/Events/Listeners/GameEventListeners.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AgToolkit.Core.Helper.Events.Listeners
-{
-    public class StringGameEventListener : GameEventListener<string, StringGameEvent, StringUnityEvent> { }
-    public class IntGameEventListener : GameEventListener<int, IntGameEvent, IntUnityEvent> { }
-    public class FloatGameEventListener : GameEventListener<float, FloatGameEvent, FloatUnityEvent> { }
-    public class BoolGameEventListener : GameEventListener<bool, BoolGameEvent, BoolUnityEvent> { }
-
-}
