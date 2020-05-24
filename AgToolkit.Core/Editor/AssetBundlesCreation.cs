28b7b909f69c226c951acb1ca01ace94ff3417c4
AlexisAG <agay@amiltone.com>
2020-05-24 13:00:59 +0200

    AssetBundle creation
    Feature request #15 : https://github.com/AlexisAG/AgToolkit/issues/15

---
+++ AgToolkit.Core/Editor/AssetBundlesCreation.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetBundlesCreation
+{
+#if UNITY_EDITOR
+    private static string _AssetBundleDirectory = "Assets/StreamingAssets";
+
+    [MenuItem("Assets/Create/AgToolkit/AssetBundle/CreateBundleName")]
+    public static void CreateBundleName() 
+    {
+        string dirPath = AssetDatabase.GetAssetPath(Selection.GetFiltered<Object>(SelectionMode.Assets)[0]);
+
+        if (string.IsNullOrEmpty(dirPath)) return;
+
+        string dirName = new DirectoryInfo(dirPath).Name;
+
+        foreach (string asset in Directory.GetFiles(dirPath, "*.asset"))
+        {
+            AssetImporter.GetAtPath(asset).assetBundleName = dirName;
+        }
+    }
+
+    [MenuItem("AgToolkit/AssetBundle/Build")]
+    public static void BuildAllAssetBundles()
+    {
+        if (!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(_AssetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(_AssetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+#endif
+}
